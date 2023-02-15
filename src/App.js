c06da25ee1e8a431c140ff302a39f7a8f8d03641
yashwanthg7 <yashwanth18183@gmail.com>
2023-02-15 18:06:28 +0530

    Checkpoint 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React, { Component, useState } from 'react';
+class App extends Component {
+    render() {
+        return (
+            
+            <>
+                <h1>Netflix Clone</h1>
+            </>
+        )
+    }
+}
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+
+  }, []);
+
+  return (
+   
+  );
+};
+
+export default Navbar;
